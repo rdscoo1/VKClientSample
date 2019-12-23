02f22acb8744fe3f22d1b466fb74313dd622d32d
Roman Khodukin <romakhodukin@gmail.com>
2019-12-23 23:50:55 +0300

    initial

---
+++ VKClientSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKClientSample
+//
+//  Created by Roman Khodukin on 23.12.2019.
+//  Copyright © 2019 Roman Khodukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
