ef06a62bc07ee17095eddde7527682ea8004203a
Roman Khodukin <romakhodukin@gmail.com>
2019-12-24 18:14:32 +0300

    done layout for all screens without scrollView

---
+++ VKClientSample/Utilities/Constants/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  LoginApp
+//
+//  Created by Roman Khodukin on 27.10.2019.
+//  Copyright © 2019 Roman Khodukin. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    enum Colors {
+        static let gray = UIColor(hex: "93A8B3")
+        static let dark = UIColor(hex: "3C425B")
+        static let vkTheme = UIColor(hex: "#4680C2")
+    }
+}
+
+
